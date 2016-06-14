862d36e8348864a980d8f01c48d74f1f8406439c
KacieW <kaciewang428@gmail.com>
2016-06-14 10:58:49 -0400

    Create EX.1.1.5.java

---
+++ EX.1.1.5.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class Exericise_1_1 {
+  public static void main(String[] args){
+	  double x = Double.parseDouble(args[0]); 
+	  double y = Double.parseDouble(args[1]);
+	  if((x>0&&x<1)&&(y>0&&y<1)){
+		  System.out.println("true");
+	  }
+	  else{
+		  System.out.println("false");
+	  }
+	  
+  }
+}
